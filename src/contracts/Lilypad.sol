314086a3b54c1269aabd46f25e15a14caef5c367
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2023-09-11 23:01:55 +0530

    lilypad contracts

---
+++ src/contracts/Lilypad.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+import "./LilypadEventsUpgradeable.sol";
+import "./LilypadCallerInterface.sol";
+
+contract Lilypad is LilypadCallerInterface {
+    LilypadEventsUpgradeable bridge;
+
+    event JobCompleted(
+        uint256 indexed jobId,
+        address from,
+        LilypadResultType resultType,
+        string result
+    );
+
+    event JobCanceled(
+        uint256 indexed jobId,
+        address from,
+        string result
+    );
+
+    constructor(address bridgeContract) {
+        bridge = LilypadEventsUpgradeable(bridgeContract);
+    }
+
+    function runJob(string memory _spec) external payable  {
+        uint256 lilypadFee = bridge.getLilypadFee();
+
+        bridge.runLilypadJob{value: lilypadFee}(address(this), _spec, uint8(LilypadResultType.CID));
+    }
+
+    function getLilypadFee() private view returns (uint256) {
+        return bridge.getLilypadFee();
+    }
+
+    // Callbacks
+    function lilypadFulfilled(
+        address _from,
+        uint _jobId,
+        LilypadResultType _resultType,
+        string calldata _result
+    ) external override {
+        emit JobCompleted(_jobId, _from, _resultType, _result);
+    }
+
+    function lilypadCancelled(
+        address _from,
+        uint256 _jobId,
+        string calldata _errorMsg
+    ) external override {
+        emit JobCanceled(_jobId, _from, _errorMsg);
+    }
+}
